b2e478325bc13f4bb296d9c620e0fdbe534cecb7
budwhite <budwhite@gmail.com>
2013-05-07 20:21:54 -0700

    makes changes to landing page and user edit page

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,10 @@ class Child < ActiveRecord::Base
   attr_accessible :first_name, :grade, :last_name, :name, :teacher, :gender
 
   belongs_to :user
+
+  validates :name, :grade, :teacher, presence: true
+
+  GRADE_TYPES = %w(K 1 2 3 4 5 6)
+  validates_inclusion_of :grade, in: GRADE_TYPES, message: "{{value}} must be in #{GRADE_TYPES.join ', '}" 
+
 end
