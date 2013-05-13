9661fbe65d3b39c1eb6bc7e5457684e9b24d6655
budwhite <budwhite@gmail.com>
2013-05-13 10:42:09 -0700

    adds email settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,9 +48,6 @@ Love::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable threaded mode
   # config.threadsafe!
 
@@ -64,4 +61,21 @@ Love::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'pickupmykid.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "pickupmykid.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_SMTP_USERNAME"],
+    password: ENV["GMAIL_SMTP_PASSWORD"]
+  }
 end
